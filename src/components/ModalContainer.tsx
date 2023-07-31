cb7a898a8e64e6cbaf9c965f797d8e34c0a98875
taeheonk <xbox1720@naver.com>
2023-07-31 19:19:04 +0900

    fix : modal container 수정

--- src/components/ModalContainer.tsx
+++ src/components/ModalContainer.tsx
@@ -3,14 +3,10 @@ import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
 
 // {children}> 태그 안에 원하시는 레이아웃 작성하시면 됩니다.
-export default function ContainerModal({ children }) {
-    const [open, setOpen] = React.useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+export default function ModalContainer({ open, handleClose, children }) {
 
     return (
         <div>
-            <Button onClick={handleOpen}>Open modal</Button>
             <Modal
                 open={open}
                 onClose={handleClose}
