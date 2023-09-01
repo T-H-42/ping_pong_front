63a6998799ad4acd504dbd17f508d5e00fb142f0
Namik Hwang <nhwang@c5r1s4.42seoul.kr>
2023-09-01 15:21:31 +0900

    fix:chatRoomInvite

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -4,8 +4,10 @@ import axios from 'axios';
 import { getJwtCookie } from '../../api/cookies';
 import { useMutation, useQuery } from 'react-query';
 import fetchProfileData from '../../components/fetchProfileData'
-import { ProfileHeader, ProfileGameHistory, ProfileAchievements } from '../../components/ProfileComponents'
 import { Button, Stack, Box, Typography, Switch, TextField } from '@mui/material';
+
+import { ProfileHeader, ProfileGameHistory, ProfileAchievements } from '../../components/ProfileComponents'
+import ModalRoomInvitationReceiver from '../../components/ModalRoomInvitationReceiver'
 import { SocketContext } from '../../api/SocketContext';
 
 // const fetchChangeNickName = (nickname) =>
@@ -64,57 +66,69 @@ const fetchChangeImage = (formData) =>
 const MyPage = () => {
   const navigate = useNavigate();
   const { pingpongSocket, chatSocket, gameSocket } = useContext(SocketContext);
+  const [roomName, setRoomname] = useState('');
+  const [sender, setSender] = useState('');
+  const [openInvitation, setOpenInvitation] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     chatSocket.off('ft_getfriendlist');
+    chatSocket.on('ft_invitechat', (res: any) => {
+      console.log('ft_invitechat on: ', res);
+      setRoomname(res.index);
+      setSender(res.sender);
+      setOpenInvitation(true);
+  });
   })
 
   const fetchChangeNickName = (nickname) =>
-  axios.post(`http://${process.env.REACT_APP_IP_ADDRESS}:4000/user/nickname`,
-    { nickname },
-    {
-      withCredentials: true,
-      headers: {
-        Authorization: `Bearer ${getJwtCookie('jwt')}`,
-      }
-    },
-  )
+    axios.post(`http://${process.env.REACT_APP_IP_ADDRESS}:4000/user/nickname`,
+      { nickname },
+      {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${getJwtCookie('jwt')}`,
+        }
+      },
+    )
   // .then((res) => {
   //   console.log('/user/nickname 요청 성공: ', res);
   //   chatSocket.emit('ft_changenickname', (res: any) => {
   //     console.log('ft_changenickname emit: ', res);
   //   });
   // })
-    // .catch((err) => {
-    //   console.log(`/user/nickname 요청 실패: ${err}`);
-    //   console.log(err);
-    //   if(err?.response?.status === 400){
-    //     alert("닉네임 수정에 실패하였습니다.")
-    //     navigate('/')
-    //   }else if(err?.response?.status === 500)
-    //     alert("잘못된 접근입니다.")
-    //     navigate('/')
-    // }
-    // );
+  // .catch((err) => {
+  //   console.log(`/user/nickname 요청 실패: ${err}`);
+  //   console.log(err);
+  //   if(err?.response?.status === 400){
+  //     alert("닉네임 수정에 실패하였습니다.")
+  //     navigate('/')
+  //   }else if(err?.response?.status === 500)
+  //     alert("잘못된 접근입니다.")
+  //     navigate('/')
+  // }
+  // );
 
   const [username, setUsername] = useState(localStorage.getItem('username'));
 
   const { data: userInfo, remove } = useQuery(
     ['userInfo', username],
     () => fetchProfileData(username),
-    { suspense: true,
-      onError: (err : any) => {
-      // 에러가 발생한 경우에 대한 처리
-      if (err?.response?.status === 401) {
-        // 인증 오류(401)인 경우 리다이렉션을 수행합니다.
-        navigate('/login');
-      } else {
-        // 기타 다른 예외적인 상황에 대한 처리
-        console.error('프로필 데이터 가져오기 오류:', err);
-        // 추가적인 에러 핸들링 로직을 구현합니다.
-      }},}
+    {
+      suspense: true,
+      onError: (err: any) => {
+        // 에러가 발생한 경우에 대한 처리
+        if (err?.response?.status === 401) {
+          // 인증 오류(401)인 경우 리다이렉션을 수행합니다.
+          navigate('/login');
+        } else {
+          // 기타 다른 예외적인 상황에 대한 처리
+          console.error('프로필 데이터 가져오기 오류:', err);
+          // 추가적인 에러 핸들링 로직을 구현합니다.
+        }
+      },
+    }
   );
-  
+
   const [newUserInfo, setNewUserInfo] = useState(
     {
       nickname: userInfo?.username,
@@ -122,27 +136,26 @@ const MyPage = () => {
       image: ""
     }
   );
-  
+
   const { mutateAsync: mutateUserName } = useMutation(fetchChangeNickName, {
-    onSuccess: (res : any) => {
+    onSuccess: (res: any) => {
       setUsername(newUserInfo.nickname);
       remove();
       localStorage.setItem('username', newUserInfo.nickname);
       console.log('/user/nickname 요청 성공: ', res);
     },
-    onError: (err : any) => {
+    onError: (err: any) => {
       console.log('???? ', err);
 
       setNewUserInfo({ ...newUserInfo, nickname: userInfo.username });
-      if(err?.response?.data?.statusCode === 400){
+      if (err?.response?.data?.statusCode === 400) {
         alert(err.response.data.message);
         // navigate('/')
-      }else if(err?.response?.data?.statusCode === 500){
+      } else if (err?.response?.data?.statusCode === 500) {
         alert("잘못된 접근입니다.")
         navigate('/')
       }
-      else if(err?.response?.data?.statusCode === 401)
-      {
+      else if (err?.response?.data?.statusCode === 401) {
         navigate('/')
       }
       // alert('닉네임을 변경할 수 없습니다');
@@ -191,7 +204,7 @@ const MyPage = () => {
     if (userInfo.two_factor_authentication_status !== newUserInfo.two_factor_authentication_status)
       await mutateTwoFactorAuthenticationStatus(newUserInfo.two_factor_authentication_status).catch((err) => console.log(err));;
 
-      
+
     chatSocket.emit('ft_changenickname', (res: any) => {
       console.log('ft_changenickname emit: ', res);
     });
@@ -213,8 +226,13 @@ const MyPage = () => {
     navigate('/main');
   }
 
+  const handleClose = () => {
+    setOpenInvitation(false);
+  };
+
   return (
     <>
+      <ModalRoomInvitationReceiver isOpen={openInvitation} onClose={handleClose} title={'채팅방 초대'} roomName={roomName} sender={sender} />
       <ProfileHeader imageUrl={userInfo.image_url} userName={username} ladderLv={userInfo.ladder_lv} />
       <ProfileGameHistory
         username={username}
